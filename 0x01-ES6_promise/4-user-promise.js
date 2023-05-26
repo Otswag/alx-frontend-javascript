d722c4f7656e0004e6e164e72471e747fdce2ced
Otswag <106761383+Otswag@users.noreply.github.com>
2023-05-27 00:33:22 +0100

    Updated UserSignUp

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,6 +1,7 @@
-export default function signUpUser(firstName, lastName) {
+function signUpUser(firstName, lastName) {
   return Promise.resolve({
-    firstName: `${firstName}`,
-    lastName: `${lastName}`,
+    firstName: firstName,
+    lastName: lastName,
   });
 }
+export default signUpUser;

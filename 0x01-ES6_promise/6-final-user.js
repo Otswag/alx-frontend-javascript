5fb85e0007c0d0348ba80660548578339a56a6cd
Otswag <106761383+Otswag@users.noreply.github.com>
2023-05-27 00:50:50 +0100

    Updated final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,17 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
-
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((values) => {
-    const arr = [];
-    for (const item of values) {
-      arr.push({ status: item.status, value: item.value || item.reason });
-    }
-    return arr;
-  });
+import { signUpUser } from './4-user-promise';
+import { uploadPhoto } from './5-photo-reject';
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+  let result;
+  try {
+    result = await Promise.allSettled([signUpPromise, uploadPromise]);
+  } catch (error) {
+    result = error;
+  }
+  return result.map((promiseResult) => ({
+    status: promiseResult.status,
+    value: promiseResult.status === 'fulfilled' ? promiseResult.value : promiseResult.reason,
+  }));
 }
+export default handleProfileSignup;
